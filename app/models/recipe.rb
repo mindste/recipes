33a3a3f2c3ec7c2f503cf38643cdc66d77656e5d
robote <2233628310@qq.com>
2017-07-01 23:42:46 +0800

    add  ingredient  and  direction  form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ApplicationRecord
+  has_many  :ingredients
+  has_many  :directions
+  validates  :title, :description, :image, presence: true
+  
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
   mount_uploader :image, ImageUploader
 end
